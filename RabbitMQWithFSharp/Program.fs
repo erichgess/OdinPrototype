3d4794995cf1ca3fbe1c0391b31b46d41e10b6fa
Erich Ess <erich.g.ess@gmail.com>
2014-01-12 14:02:10 -0800

    Removed some practice code I wrote (this code published 1000 messages to the queue).  It's not needed for this project.

--- RabbitMQWithFSharp/Program.fs
+++ RabbitMQWithFSharp/Program.fs
@@ -12,12 +12,8 @@ let main argv =
 
     channel.QueueDeclare( "fsharp-queue", false, false, false, null) |> ignore
 
-    let message = Encoding.UTF8.GetBytes("Now is the time for all good men to come to the aid of their country.Now is the time for all good men to come to the aid of their country.Now is the time for all good men to come to the aid of their country.Now is the time for all good men to come to the aid of their country.Now is the time for all good men to come to the aid of their country.Now is the time for all good men to come to the aid of their country.Now is the time for all good men to come to the aid of their country.Now is the time for all good men to come to the aid of their country.Now is the time for all good men to come to the aid of their country.Now is the time for all good men to come to the aid of their country.Now is the time for all good men to come to the aid of their country.Now is the time for all good men to come to the aid of their country.")
+    let message = Encoding.UTF8.GetBytes("Now is the time for all good men to come to the aid of their country.")
     channel.BasicPublish("", "fsharp-queue", null, message);
-
-    let messages = { 1 .. 1000 } |> Seq.map( fun i -> i.ToString() )
-    messages |> Seq.map (fun m -> Encoding.UTF8.GetBytes(m))
-             |> Seq.iter (fun m -> channel.BasicPublish("", "fsharp-queue", null, message); )
 
     channel.Close()
     connection.Close()
