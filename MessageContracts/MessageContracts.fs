4ccad2961e3b5f5c06d7914d5333241d7ad47c58
Erich Ess <erich.g.ess@gmail.com>
2014-01-24 19:38:34 -0800

    Added a function to the Message type which will serialize it to XML.

--- MessageContracts/MessageContracts.fs
+++ MessageContracts/MessageContracts.fs
@@ -1,14 +1,23 @@
 ﻿namespace MessageContracts
 
-
+open System.Reflection
+open Microsoft.FSharp.Reflection
 open System.Runtime.Serialization
 open System.IO
+open System.Text  
+open System.Xml
+open System.Runtime.Serialization
 open System.Runtime.Serialization.Formatters.Binary
 
+[<KnownType("GetKnownTypes")>]
 type Message =
     | TypeA of int
     | TypeB of string
     with
+    static member GetKnownTypes() =
+        typedefof<Message>.GetNestedTypes(BindingFlags.Public ||| BindingFlags.NonPublic)
+        |> Array.filter FSharpType.IsUnion
+
     static member Decode(packet : byte[]) =
         use ms = new MemoryStream(packet)
         let bf = new BinaryFormatter()
@@ -20,3 +29,9 @@ type Message =
         let bf = new BinaryFormatter()
         bf.Serialize(ms, this)
         ms.GetBuffer()
+
+    member this.XmlEncode() =
+        let xmlSerializer = DataContractSerializer(typeof<Message>); 
+        let sb = new StringBuilder()
+        xmlSerializer.WriteObject(new XmlTextWriter(new StringWriter(sb)), this)
+        sb.ToString()
