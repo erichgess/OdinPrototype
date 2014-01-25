317191d7bee70bfad25171ace5b55a9f0e4dd720
Erich Ess <erich.g.ess@gmail.com>
2014-01-24 19:14:00 -0800

    Added a library with message contracts

---
+++ MessageContracts/MessageContracts.fs
@@ -0,0 +1,22 @@
+﻿namespace MessageContracts
+
+
+open System.Runtime.Serialization
+open System.IO
+open System.Runtime.Serialization.Formatters.Binary
+
+type Message =
+    | TypeA of int
+    | TypeB of string
+    with
+    static member Decode(packet : byte[]) =
+        use ms = new MemoryStream(packet)
+        let bf = new BinaryFormatter()
+        bf.Deserialize(ms) 
+        |> unbox<Message>
+
+    member this.Encode() =
+        use ms = new MemoryStream()
+        let bf = new BinaryFormatter()
+        bf.Serialize(ms, this)
+        ms.GetBuffer()
