afd2bb9f01909332a118e2a115a8c96ae8d2abf1
Erich Ess <erich.g.ess@gmail.com>
2014-01-24 19:48:36 -0800

    added a console project which will allow me to easily test serialization and deserialization.

---
+++ ConsoleTester/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open MessageContracts
+
+[<EntryPoint>]
+let main argv = 
+    let t = TypeA(1)
+
+    printfn "%s" (t.XmlEncode())
+    0 // return an integer exit code
